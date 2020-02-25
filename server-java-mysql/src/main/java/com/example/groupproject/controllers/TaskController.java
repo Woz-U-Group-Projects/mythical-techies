58a7c024c42d4f21ca08391607f0e462f48917a8
Remon <remonallen@yahoo.com>
2020-02-25 12:52:19 -0500

    new changes

--- server-java-mysql/src/main/java/com/example/groupproject/controllers/TaskController.java
+++ server-java-mysql/src/main/java/com/example/groupproject/controllers/TaskController.java
@@ -3,7 +3,6 @@ package com.example.groupproject.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- server-java-mysql/src/main/java/hello/GreetingController.java
+++ server-java-mysql/src/main/java/hello/GreetingController.java
@@ -2,6 +2,7 @@ package hello;
 
 // import all necessary tools
 import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
