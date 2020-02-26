02bd828aa80f4d88a841f28128330e12243a9e4b
Remon <remonallen@yahoo.com>
2020-02-26 17:44:19 -0500

    new 2.26 11:44 pm

---
+++ Techies/src/test/java/com/techies/TechiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
