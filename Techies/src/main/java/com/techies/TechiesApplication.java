e6630704265df2f1e37ce406f2704f59c2cad7c6
Remon <remonallen@yahoo.com>
2020-02-26 05:38:59 -0500

    2.26 12PM

---
+++ Techies/src/main/java/com/techies/TechiesApplication.java
@@ -0,0 +1,13 @@
+package com.techies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechiesApplication.class, args);
+	}
+
+}
